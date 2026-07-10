4f95c7e52704fa926d1a3dca687b608a6f1c8fd0
Kurapati-Sai-Suhas <kurapatisai1683@gmail.com>
2026-07-10 23:25:22 +0530

    feat: admin search and filters for Questions and CodeSubmissions

--- backend/LearnLM/groups/admin.py
+++ backend/LearnLM/groups/admin.py
@@ -10,7 +10,19 @@ from .models import (
 admin.site.register(StudyGroup)
 admin.site.register(StudyMaterial)
 admin.site.register(Topic)
-admin.site.register(Question)
 admin.site.register(CodingPortal)
-admin.site.register(CodeSubmission)
 admin.site.register(UserCodingProfile)
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    list_display = ['title', 'topic', 'base_difficulty']
+    list_filter = ['topic']
+
+
+@admin.register(CodeSubmission)
+class CodeSubmissionAdmin(admin.ModelAdmin):
+    search_fields = ['question__title', 'user__username']
+    list_display = ['user', 'question', 'status', 'language', 'submitted_at']
+    list_filter = ['status', 'language']
